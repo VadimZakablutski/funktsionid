c239bc39c27c94d432a8f58bdff9c03490ad9fed
Opilane TTHK <opilane@tthk.ee>
2021-11-11 10:01:38 +0200

    Add project files.

---
+++ funktsionid.py
@@ -0,0 +1,31 @@
+from module import *
+while True:
+    print("Funktsioonid".center(50,"+"))
+    v=input("arithmetic- A, \nis_year_leap- B, \nsquare- C, \nseason- D, \nbank- E")
+    if v.upper()=="A":
+        a=float(input("Arv 1: "))
+        b=float(input("Arv 2: "))
+        c=input("Tehe: ")
+        result=arithmetic(a,b,c)
+        print(result)
+    elif v.upper()=="B":
+        year=int(input("Sisesta aasta: "))
+        result=is_year_leap(year)
+        print(result)
+    elif v.upper()=="C":
+        kv=int(input("Sisestage ruudu külg: "))
+        result=square(kv)
+        print(result)
+    elif v.upper()=="D":
+        kuu=int(input("Sisestage kuu number: "))
+        result=season(kuu)
+        print(result)
+    elif v.upper()=="E":
+        a=float(input("Sisestage sissemakse summa: "))
+        years=int(input("Mitu aastat on möödunud?"))
+        result=bank(a,years)
+        print(result)
+    elif v.upper()=="F":
+        a=int(input("Sisestage number: "))
+        result=is_prime(a)
+        print(result)
---
+++ module.py
@@ -0,0 +1,58 @@
+def arithmetic(a: float,b:float,c=str):
+    """Lihtne kalkulaator
+    + - liitamine
+    - - lahutamine
+    * - korrutamine
+    / - jagamine
+    :param float a: Esimene arv
+    :param float b: Teine arv
+    :param str c: Aritmeetiline tehing
+    :rtype float:
+    """
+    if c=="+":
+        r=a+b
+    elif c=="-":
+        r=a-b
+    elif c=="*":
+        r=a*b
+    elif c=="/":
+        if b!=0:
+            r=a/b
+        else:
+            print("Div0")
+    else:
+        print("Viga!")
+    return("")
+def is_year_leap(year:int):
+    if year%4==0:
+        print("True")
+    else:
+        print("False")
+    return("")
+def square(kv:float):
+    return(4*kv, kv**2, (2*kv**2)**.5)
+    return("")
+def season(kuu:int):
+    if kuu==12:
+        print("Зима")
+    elif 0<kuu<3:
+        print("Зима")
+    elif 2<kuu<6:
+        print("Весна")
+    elif 5<kuu<9:
+        print("Лето")
+    elif 8<kuu<12:
+        print("Осень")
+    else:
+        print("Viga!")
+    return
+def bank(a:float,years:int):
+    for _ in range(years):
+        a=((1.1*1/100)*a)*100
+    print("Ваш баланс:",a)
+    return("")
+def is_prime(a:int):
+    b=2
+    while a%b!=0:
+        b+=1
+    return b==a
